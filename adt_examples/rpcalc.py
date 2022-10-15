e9e4189703d14db9fa2dfdf5b2cdf108635784d9
Filip Weidler <filip.weidler18@imperial.ac.uk>
2022-10-15 16:43:15 +0200

    flake8 improvements

--- adt_examples/rpcalc.py
+++ adt_examples/rpcalc.py
@@ -1,15 +1,21 @@
+"""
+This module implements a reverse polish calculator.
+"""
+
+
 import math
 
 
 class RPCalc:
+    """Class to implement reverse polish calculator."""
 
     def __init__(self):
-        """Initialise stack for calculator"""
+        """Initialise stack for calculator."""
         self.stack = []
         self.operators = ("+", "-", "*", "/", "sin", "cos")
 
     def push(self, n):
-        """Implement method for pushing to stack"""
+        """Implement method for pushing to stack."""
         if n not in self.operators:
             self.stack.append(n)
         else:
@@ -37,13 +43,13 @@ class RPCalc:
                 self.stack.append(num1 / num2)
 
     def pop(self):
-        """Implement method to pop stack"""
+        """Implement method to pop stack."""
         return self.stack.pop()
 
     def peek(self):
-        """Implement method to peek at stack"""
+        """Implement method to peek at stack."""
         return self.stack[-1]
 
     def __len__(self):
-        """Method for determining number of elements in stack"""
+        """Method for determining number of elements in stack."""
         return len(self.stack)
